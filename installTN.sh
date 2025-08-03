a2462d04d2d20b7bf9e90d13538f4e59e88cca51
Keisha_Pradana <sonyxzsbr@gmail.com>
2025-08-03 12:43:01 +0700

    Update installTN.sh

--- installTN.sh
+++ installTN.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "\e[1;34m📦 Menginstall dependensi...\e[0m"
+echo -e "\e[1;32m📦 Menginstall dependensi...\e[0m"
 pkg install -y python ffmpeg
 pip install -U yt-dlp
 
-echo -e "\e[1;34m📥 Mengambil TNmp3dl dari GitHub...\e[0m"
+echo -e "\e[1;32m📥 Mengambil TNmp3dl dari GitHub...\e[0m"
 curl -o TNmp3dl https://raw.githubusercontent.com/KeishaPradana/TN_ytmp3-script/main/TN_ytmp3dl
 
 chmod +x TNmp3dl
@@ -18,17 +18,8 @@ blue='\e[1;34m'
 red='\e[0;31m'
 reset='\e[0m'
 
-# Fungsi cetak tengah
-center() {
-    teks="$1"
-    lebar=$(tput cols)
-    panjang=${#teks}
-    spasi=$(( (lebar - panjang) / 2 ))
-    printf "%*s\e%s%s\e[0m\n" $spasi "" "${2}" "$teks"
-}
-
 # Tampilkan pesan
-center "✅ Instalasi selesai!" "1;32m"
-center "🌍 By.TN_KeishaPradana 📡" "1;34m"
-center "⚠️ Pakai Script dengan bijak ⚠️" "0;31m"
-center "Mengunduh lagu tanpa izin dari pemilik hak cipta adalah tindakan Ilegal ,Tidak untuk tujuan komersial hanya untuk di dengar pribadi" "1;31m"
+echo -e "\e[1;32m✅ Instalasi selesai!\e[0m"
+echo -e "\e[1;34m🌍 By.TN_KeishaPradana 📡\e[0m"
+echo -e "\e[1;31m⚠️ Pakai Script dengan bijak ⚠️\e[0m"
+echo -e "\e[0;31Mengunduh lagu tanpa izin dari pemilik hak cipta adalah tindakan Ilegal ,Tidak untuk tujuan komersial hanya untuk di dengar pribadi\e[0m"
